c9eb07a46724446c02fad616bcb0ba654c463327
Bastian Gerner <Bastian.Gerner@gmail.com>
2025-02-12 00:40:28 +0100

    Updated .gitignore to exclude unnecessary files

--- backend/dist/routes/person.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const person_controller_1 = require("../controllers/person.controller");
-const router = (0, express_1.Router)();
-router.get("/", person_controller_1.getAllPersons);
-router.post("/", person_controller_1.addPerson);
-exports.default = router;
